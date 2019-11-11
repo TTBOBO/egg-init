946d1a8e855dfb5e845d9700eaf405337881f08d
v_aibotu <v_aibotu@tencent.com>
2019-11-11 15:57:07 +0800

    更新文件

--- admin/BasicPlugin.js
+++ admin/BasicPlugin.js
@@ -13,10 +13,10 @@ class BasicPlugin {
       const timer = parseInt(endTime / 1000) - parseInt(startTime / 1000)
       console.log(timer)
       console.log('打包结束')
-      fs.writeFileSync(
-        '../admin-server/index.html',
-        fs.readFileSync('./dist/index.html')
-      )
+      // fs.writeFileSync(
+      //   '../admin-server/index.html',
+      //   fs.readFileSync('./dist/index.html')
+      // )
       fs.createReadStream('./dist/static.zip', {
         highWaterMark: 1024 * 1024 * 5
       }).pipe(fs.createWriteStream('../admin-server/static.zip'))
